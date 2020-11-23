9f0d5b9104861a697cd47b49b9ad398447993690
zeeket <3qvm@tutanota.com>
2020-11-23 23:08:24 +0200

    toka testi

--- Verkkokauppa/src/test/java/ohtu/verkkokauppa/VerkkokauppaTest.java
+++ Verkkokauppa/src/test/java/ohtu/verkkokauppa/VerkkokauppaTest.java
@@ -48,4 +48,32 @@ public class VerkkokauppaTest {
         // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
         verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(),eq(5));   
     }
+
+    @Test
+    public void kahdenOstoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
+        // luodaan ensin mock-oliot
+
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // määritellään että tuote numero 2 on peruna jonka hinta on 1 ja saldo 100
+        when(varasto.saldo(2)).thenReturn(100); 
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "peruna", 1));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.lisaaKoriin(2);     // ostetaan tuotetta numero 1 eli perunaa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(eq("pekka"), eq(42), eq("12345"), anyString(),eq(6));   
+    }
 }
