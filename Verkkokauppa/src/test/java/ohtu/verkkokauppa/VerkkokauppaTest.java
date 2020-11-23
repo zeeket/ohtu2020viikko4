d4035b43f75129882afdca9618e19bc52dfc15b9
zeeket <3qvm@tutanota.com>
2020-11-23 22:16:39 +0200

    eka testi läpi

---
+++ Verkkokauppa/src/test/java/ohtu/verkkokauppa/VerkkokauppaTest.java
@@ -0,0 +1,52 @@
+package ohtu;
+
+import ohtu.verkkokauppa.*;
+import org.junit.Before;
+import org.junit.Test;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class VerkkokauppaTest {
+    Pankki pankki;
+    Kauppa kauppa;
+    Viitegeneraattori viite;
+    Varasto varasto;
+    // ...
+
+    @Before
+    public void setUp() {
+        pankki = mock(Pankki.class);
+        viite = mock(Viitegeneraattori.class);
+        varasto = mock(Varasto.class);
+        // ...
+    }
+
+    @Test
+    public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
+        // luodaan ensin mock-oliot
+
+        // määritellään että viitegeneraattori palauttaa viitten 42
+        when(viite.uusi()).thenReturn(42);
+
+        // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10); 
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+        // sitten testattava kauppa 
+        Kauppa k = new Kauppa(varasto, pankki, viite);              
+
+        // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+        // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+        verify(pankki).tilisiirto(anyString(), anyInt(), anyString(), anyString(),anyInt());   
+        // toistaiseksi ei välitetty kutsussa käytetyistä parametreista
+    }
+}
